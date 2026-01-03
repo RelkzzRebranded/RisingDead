5a32cb34db703dfae9199737d5b961bb4f563fa6
RelkzzRebranded <130754062+RelkzzRebranded@users.noreply.github.com>
2026-01-03 23:08:08 +0800

    Update InfAmmo.lua

--- InfAmmo.lua
+++ InfAmmo.lua
@@ -1,16 +1,21 @@
+pcall(function()
+	local executorname, version = identifyexecutor()
+	executorname = executorname:upper()
+	
+	if string.match(executorname, "XENO") or string.match(executorname, "SOLARA") then
+		game.Players.LocalPlayer:Kick("GET A BETTER FUCKING EXECUTOR YOU FUCKING MONGOLIAN FUCK.")
+		while true do end -- fuck you if you have solara
+	end
+end)
+
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 local DataModule = require(LocalPlayer.DataModule)
 
 local OriginalGetModCharacter = DataModule.GetModCharacter
 
--- TerminalVibes the GOAT.
--- blud told me to use metatables instead of doing it by loops
--- so i did it and it was hard at first but oh boy it was fun
+-- Alr i changed methods, it will no longer fuck up your game.
 DataModule.GetModCharacter = function(...)
-    -- some executors identity of 8 fucks up the way some of roblox's [C] functions work
-    -- so i had to use setthreadidentity here
-    setthreadidentity(2) 
     local CharacterData = OriginalGetModCharacter(...)
 
     if CharacterData and CharacterData.EquippedItem then
@@ -18,44 +23,11 @@ DataModule.GetModCharacter = function(...)
         local Item = DataModule:GetItemClass(ID)
         local Properties = Item.Properties
         local RealProperties = DataModule.GetItemById(ID).Values
-        local oldProperties = getmetatable(Properties)
-        local oldRealProperties = getmetatable(RealProperties)
-        -- OMG WOWW!!!
-        -- USE CASES OF METATABLES?!!?
-        -- IM WET! (pause, wtf bro)
-        setmetatable(RealProperties, {
-            __index = function(t, k)
-                if k == "A" then
-                    return t.MA
-                end
-                -- yes. you can chain it.
-                if oldRealProperties and oldRealProperties.__index then
-                    if type(oldRealProperties.__index) == "table" then
-                        return oldRealProperties.__index[k]
-                    else
-                        return oldRealProperties.__index(t, k)
-                    end
-                end
-            end
-        })
-
-        setmetatable(Properties, {
-            __index = function(t, k)
-                if k == "Ammo" then
-                    return t.MaxAmmo
-                end
-
-                if oldProperties and oldProperties.__index then
-                    if type(oldProperties.__index) == "table" then
-                        return oldProperties.__index[k]
-                    else
-                        return oldProperties.__index(t, k)
-                    end
-                end
-            end
-        })
+        -- turns out doing metatables lags tf out of your game
+        -- since the game repeatedly calls `GetModCharacter` alot
+        RealProperties.MA = 99999999
+        Properties.MaxAmmo = 99999999 
     end
-    setthreadidentity(8)
 
     return CharacterData
 end
